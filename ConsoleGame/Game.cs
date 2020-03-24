681af1c47e2c0b5cdbefa8dc74ce9ec7034be91e
Primula157 <zm157@yandex.ru>
2020-03-24 17:19:50 +0300

    Добавьте файлы проекта.

---
+++ ConsoleGame/Game.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace code_cademy
+{
+    class Game
+    {
+
+        // move cursor 
+        public static void UpdatePosition(string keyPressed, out int xCoordinate, out int yCoordinate)
+        {
+            yCoordinate = 0;
+            xCoordinate = 0;
+            switch (keyPressed)
+            {
+                case "DownArrow":
+                    yCoordinate++;
+                    break;
+                case "UpArrow":
+                    yCoordinate--;
+                    break;
+                case "LeftArrow":
+                    xCoordinate--;
+                    break;
+                case "RightArrow":
+                    xCoordinate++;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // change cursor method. for example, if you move left cursor is "<". 
+        // ">" for right and etc
+        // this method returns char
+
+        /*public static char UpdateCursor(string keyPressed)
+        {
+            char cursor = ' ';
+            switch(keyPressed)
+            {
+                case "DownArrow":
+                    cursor = 'V';
+                    break;
+                case "UpArrow":
+                    cursor = '^';
+                    break;
+                case "LeftArrow":
+                    cursor = '<';
+                    break;
+                case "RightArrow":
+                    cursor = '>';
+                    break;
+                default:
+                    break;
+            }
+            return cursor;
+        }*/
+
+        // change cursor method. for example, if you move left cursor is "<". 
+        // ">" for right and etc
+        //thid method returns string
+
+        public static string UpdateCursor(string keyPressed)
+        {
+            string cursor = " ";
+            switch (keyPressed)
+            {
+                case "DownArrow":
+                    cursor = "V";
+                    break;
+                case "UpArrow":
+                    cursor = "^";
+                    break;
+                case "LeftArrow":
+                    cursor = "<:";
+                    break;
+                case "RightArrow":
+                    cursor = ":>";
+                    break;
+                default:
+                    break;
+            }
+            return cursor;
+        }
+
+        // set borders for console window
+
+        public static int KeepInBounds(int xyCoordinate, int maxCoordinate)
+        {
+            /*if (xyCoordinate == 1) xyCoordinate++;
+            if (xyCoordinate == maxCoordinate) xyCoordinate--;
+            return xyCoordinate;*/
+
+            if (xyCoordinate == 1) xyCoordinate = maxCoordinate - 2;
+            if (xyCoordinate == maxCoordinate) xyCoordinate = 2;
+            return xyCoordinate;
+        }
+
+        // if cursor reached "@fruit" return true
+        public static bool DidScore(int cursorXCoordinates, int cursorYCoordinates, int fruitXCoordinates, int fruitYCoordinates)
+        {
+            bool scored = (cursorXCoordinates == fruitXCoordinates && cursorYCoordinates == fruitYCoordinates) ? true : false;
+            return scored;
+        }
+    }
+}
---
+++ ConsoleGame/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+namespace code_cademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Console Game";
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.CursorVisible = false;
+            Console.Clear();
+            Console.SetWindowSize(50, 20);
+            Console.BufferHeight = 20;
+            Console.BufferWidth = 50;
+
+            var rand = new Random();
+            int rows = 20;
+            int cols = 50;
+            int characterRow = rows / 2;
+            int characterCol = cols / 2;
+            string cursor = "<:";
+            char fruit = '@';
+            int fruitRow = rand.Next(2, rows);
+            int fruitCol = rand.Next(2, cols);
+            int score = 0;
+            int colChange;
+            int rowChange;
+
+
+            while (true)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine($"Press Q to exit\nScore: {score}");
+                Console.SetCursorPosition(fruitCol, fruitRow); // set coordinates to spawn @fruit
+                Console.Write(fruit); 
+                Console.SetCursorPosition(characterCol, characterRow); //set coordinates for cursor
+                Console.Write(cursor);
+
+                var cki = Console.ReadKey(false); //
+
+
+                if (cki.Key == ConsoleKey.Q)
+                {
+                    Console.Clear();
+                    break;
+                }
+
+                string key = cki.Key.ToString();
+
+                Game.UpdatePosition(key, out colChange, out rowChange);
+                characterCol += colChange;
+                characterRow += rowChange;
+
+                cursor = Game.UpdateCursor(key);
+
+                characterCol = Game.KeepInBounds(characterCol, cols);
+                characterRow = Game.KeepInBounds(characterRow, rows);
+
+                if (Game.DidScore(characterCol, characterRow, fruitCol, fruitRow))
+                {
+                    fruitRow = rand.Next(2, rows);
+                    fruitCol = rand.Next(2, cols);
+                    score++;
+                }
+
+            }
+        }
+    }
+}
